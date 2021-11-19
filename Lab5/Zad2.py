fc054f85d94aeedbbb99390dc9d3f1cb885be0de
flapicz <01169318@pw.edu.pl>
2021-11-19 09:57:54 +0100

    Zadania

--- Lab5/Zad2.py
+++ Lab5/Zad2.py
@@ -1,18 +1,24 @@
-
-
 def load_file(file):
     dict = {}
     for line in file:
+        tempList = line.split()
+        dict[tempList[0]] = tempList[1]
+    print(dict)
+    return dict
 
+
+def check(dict):
+    x = input("Podaj liczbe")
+    print(x in dict)
+    y = x in dict
+    if y is True:
+        print(dict[x])
+    else:
+        print("Nieprawidłowa liczba")
 
 
 if __name__ == "__main__":
     file = open("midi_instruments.txt", "r")
-    load_file(file)
+    dict = load_file(file)
     file.close()
-
-
-dict = {
-    "1": "",
-    "2": ""
-}
+    check(dict)
